0730704195cab4b884370b54e0381c1a4ed3abd7
Nephthali Salam <snephthali@gmail.com>
2022-03-08 00:32:06 +0200

    update

--- src/Components/RestaurantsList/RestaurantsList.jsx
+++ src/Components/RestaurantsList/RestaurantsList.jsx
@@ -20,62 +20,71 @@ function RestaurantsList(props) {
 
             <img src={bigMouthImg} className="restaurant-image" alt="" />
 
-            <div className='restaurant-details'>
+            <div className='restaurant-details-container'>
                 <h3 className='restaurant-name'>The Big Mouth</h3>
+                <div className='restaurant-details'>
 
-                <div style={{ display: 'inline-block', fontSize: 14 }}>
-                    <div style={{ display: 'flex' }}>
-                        <div className='bold-text'>Monday:</div> 12pm - 10pm
+                    <div style={{ marginLeft: '-5rem' }}>
+                        <div className="address-container">
+                            <div className='bold-text'>Address: </div>
+                            <div>1st Respublic Road, Sandton, South Africa 2005</div>
+                        </div>
+
+                        <div className='flex-bold-text'>Status:
+                            <div className='status'>Open</div>
+                        </div>
+                        <div className='flex-bold-text'>Contact(s):
+                            <div>+27 67 791 9267</div>
+                        </div>
+
+                        {/* Number of stars components */}
+                        <StarsReviews avgRatings={avgRatings} numOfRatings={numOfRatings} />
+
+                        <div>
+                            <div className='buttons-container'>
+                                <button className='button-element' >
+                                    View Menu
+                                </button>
+                                <button className='button-element' >
+                                    Gallery
+                                </button>
+                                <button className='button-element'>
+                                    Book
+                                </button>
+                                <button className='button-element'>
+                                    Map
+                                </button>
+                            </div>
+                        </div>
                     </div>
-                    <div style={{ display: 'flex' }}>
-                        <div className='bold-text'>Tuesday:</div> 12pm - 10pm
-                    </div>
-                    <div style={{ display: 'flex' }}>
-                        <div className='bold-text'>Wednesday:</div> 12pm - 10pm
-                    </div>
-                    <div style={{ display: 'flex' }}>
-                        <div className='bold-text'>Thursday:</div> 12pm - 10pm
-                    </div>
-                    <div style={{ display: 'flex' }}>
-                        <div className='bold-text'>Friday:</div> 12pm - 10pm
-                    </div>
-                    <div style={{ display: 'flex' }}>
-                        <div className='bold-text'>Saturday:</div> 12pm - 10pm
-                    </div>
-                    <div style={{ display: 'flex' }}>
-                        <div className='bold-text'>Sunday:</div> 12pm - 10pm
+
+                    <div className='restaurant-hours'>
+
+                        <div className='bold-text'>Operating hours:</div>
+
+                        <div style={{ display: 'flex' }}>
+                            <div className='bold-text'>Monday:</div> 12pm - 10pm
+                        </div>
+                        <div style={{ display: 'flex' }}>
+                            <div className='bold-text'>Tuesday:</div> 12pm - 10pm
+                        </div>
+                        <div style={{ display: 'flex' }}>
+                            <div className='bold-text'>Wednesday:</div> 12pm - 10pm
+                        </div>
+                        <div style={{ display: 'flex' }}>
+                            <div className='bold-text'>Thursday:</div> 12pm - 10pm
+                        </div>
+                        <div style={{ display: 'flex' }}>
+                            <div className='bold-text'>Friday:</div> 12pm - 10pm
+                        </div>
+                        <div style={{ display: 'flex' }}>
+                            <div className='bold-text'>Saturday:</div> 12pm - 10pm
+                        </div>
+                        <div style={{ display: 'flex' }}>
+                            <div className='bold-text'>Sunday:</div> 12pm - 10pm
+                        </div>
                     </div>
                 </div>
-
-                <div className="address-container">
-                    <div className='bold-text'>Address: </div>
-                    <div>1st Respublic Road, Sandton, South Africa 2005</div>
-                </div>
-
-                <div className='flex-bold-text'>Status:
-                    <div className='status'>Open</div>
-                </div>
-                <div className='flex-bold-text'>Contact(s):
-                    <div>+27 67 791 9267</div>
-                </div>
-
-                <div style={{ display: 'flex' }}>
-                    {/* Number of stars components */}
-                    <StarsReviews avgRatings={avgRatings} numOfRatings={numOfRatings} />
-
-                    <div className='buttons-container'>
-                        <button className='btn-view-menu' >
-                            View Menu
-                        </button>
-                        <button className='btn-gallery' >
-                            Gallery
-                        </button>
-                        <button className='btn-book'>
-                            Book
-                        </button>
-                    </div>
-                </div>
-
             </div >
         </div >
     );
